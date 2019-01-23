42eb97c055ab79bf4b762e6f758daee52cdf38c8
DavePearce <david.pearce@ecs.vuw.ac.nz>
2019-01-23 14:59:40 +1300

    Update boogie command.
    The boogie command will now compile whiley files that are specified as a
    command-line argument.  However, these do not have access to any
    dependencies, such as the standard library.

--- src/main/java/wy2boogie/commands/BoogieCommand.java
+++ src/main/java/wy2boogie/commands/BoogieCommand.java
@@ -4,7 +4,11 @@ import wycc.WyProject;
 import wycc.cfg.Configuration;
 import wycc.cfg.Configuration.Schema;
 import wycc.lang.Command;
+import wycc.util.Logger;
+import wycc.util.Pair;
 import wyfs.lang.Path;
+import wyfs.lang.Path.Entry;
+import wyfs.lang.Path.Root;
 import wyfs.util.DirectoryRoot;
 import wyfs.util.Trie;
 import wyil.lang.WyilFile;
@@ -19,10 +23,18 @@ import java.io.PrintStream;
 import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
+import java.util.Set;
 
+import wybs.lang.Build;
+import wybs.lang.Build.Graph;
+import wybs.lang.Build.Project;
 import wybs.util.StdProject;
+import wybs.util.AbstractCompilationUnit.Name;
+import wybs.util.AbstractCompilationUnit.Tuple;
+import wyc.lang.WhileyFile;
 
 public class BoogieCommand implements Command {
 	/**
@@ -43,7 +55,8 @@ public class BoogieCommand implements Command {
 		@Override
 		public List<Command.Option.Descriptor> getOptionDescriptors() {
 			return Arrays.asList(Command.OPTION_FLAG("counterexample", "convert a Boogie counterexample (beg) file to a Whiley-like form (wyeg)", false),
-					Command.OPTION_FLAG("verbose", "generate verbose information about the build", false));
+					Command.OPTION_FLAG("verbose", "generate verbose information about the build", false),
+					Command.OPTION_STRING("output", "specific output file", "a.wyil"));
 		}
 
 		@Override
@@ -107,12 +120,72 @@ public class BoogieCommand implements Command {
 	public boolean execute(Template template) throws Exception {
 		boolean verbose = template.getOptions().get("verbose", Boolean.class);
 		boolean counterexample = template.getOptions().get("counterexample", Boolean.class);
+		String output = template.getOptions().get("output", String.class);
 		//
 		if(counterexample) {
 			return translateCounterexample(verbose,template.getArguments());
 		} else {
-			return translateWyilFile(verbose,template.getArguments());
+			List<String> files = translateAnyWhileyFiles(verbose,output,template.getArguments());
+			return translateWyilFile(verbose,files);
 		}
+	}
+
+	public List<String> translateAnyWhileyFiles(boolean verbose, String output, List<String> args) {
+		try {
+			Path.Root projectRoot = project.getBuildProject().getRoot();
+			List<String> files = new ArrayList<>();
+			List<Path.Entry<WhileyFile>> sources = new ArrayList<>();
+			// Identify and read in any source files
+			for(int i=0;i!=args.size();++i) {
+				String arg = args.get(i);
+				if(arg.endsWith(".whiley")) {
+					// Strip extension
+					arg = arg.replace(".whiley", "");
+					//	Find the source file in question
+					Path.Entry<WhileyFile> source = projectRoot.get(Trie.fromString(arg), WhileyFile.ContentType);
+					if(source == null) {
+						syserr.println("ignoring missing file " + arg);
+					} else {
+						sources.add(source);
+					}
+				} else {
+					files.add(arg);
+				}
+			}
+			// Create target for run
+			Trie id = Trie.fromString(output.replace(".wyil", ""));
+			files.add(output);
+			Path.Entry<WyilFile> target = createWyilFile(id);
+			System.out.println("GOT: " + target);
+			// Reuse code from the compile task for this purpose
+			wyc.task.CompileTask.build(Logger.NULL, project.getBuildProject(), target, sources);
+			// Done
+			return files;
+		} catch(RuntimeException e) {
+			throw e;
+		} catch (IOException e) {
+			// FIXME: need a better error reporting mechanism
+			System.err.println("internal failure: " + e.getMessage());
+			if (verbose) {
+				e.printStackTrace(System.err);
+			}
+			return Collections.EMPTY_LIST;
+		}
+	}
+
+	private Path.Entry<WyilFile> createWyilFile(Path.ID id) throws IOException {
+		Path.Root projectRoot = project.getBuildProject().getRoot();
+		Path.Entry<WyilFile> target = projectRoot.get(id, WyilFile.ContentType);
+		// Check whether target binary exists or not
+		if (target == null) {
+			// Doesn't exist, so create with default value
+			target = projectRoot.create(id, WyilFile.ContentType);
+			WyilFile wf = new WyilFile(target);
+			target.write(wf);
+			// Create initially empty WyIL module.
+			wf.setRootItem(new WyilFile.Decl.Module(new Name(id), new Tuple<>(), new Tuple<>()));
+		}
+		return target;
 	}
 
 	public boolean translateWyilFile(boolean verbose, List<String> args) {
